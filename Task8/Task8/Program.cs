e211e952e2980fa74b7e3c086920b8d8f0b3b007
BitW13 <shabolda.artem@mail.ru>
2018-10-16 20:41:02 +0300

    v1.01

--- Task8/Task8/Program.cs
+++ Task8/Task8/Program.cs
@@ -10,6 +10,9 @@ namespace Task8
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("Text");
+            //string text = Console.ReadLine();
+            //TextSeparation ts = new TextSeparation(text);
             TextSeparation ts = new TextSeparation("Товарищи! новая модель организационной деятельности представляет собой интересный эксперимент проверки новых предложений. Таким образом рамки и место обучения кадров требуют определения и уточнения системы обучения кадров, соответствует насущным потребностям. Равным образом рамки и место обучения кадров требуют определения и уточнения существенных финансовых и административных условий.");
             ts.SplitWords();
             string[] splitWords = ts.GetSplitWords();
