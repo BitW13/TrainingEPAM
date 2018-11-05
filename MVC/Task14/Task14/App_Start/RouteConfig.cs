fa38bb403f0acdfad29aa7f8b382d9abe92b7f38
BitW13 <shabolda.artem@mail.ru>
2018-11-05 12:43:29 +0400

    v.2.0

--- MVC/Task14/Task14/App_Start/RouteConfig.cs
+++ MVC/Task14/Task14/App_Start/RouteConfig.cs
@@ -13,20 +13,20 @@ namespace Task14
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "GalleryRoute",
-                url: "Gallery/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
-            );
-
+            
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
 
-            
-            
+            routes.MapRoute(
+                name: "GalleryRoute",
+                url: "Gallery/{action}/{id}",
+                defaults: new { action = "Index", id = UrlParameter.Optional }
+            );
+
+
         }
     }
 }
